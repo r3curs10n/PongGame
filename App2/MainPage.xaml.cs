43153c3df75a1e39c6e4f4abb720c1bdf8bdcabb
Shreyas Basarge <shreyasrulez@gmail.com>
2013-03-26 01:14:38 +0530

    minor changes

--- App2/MainPage.xaml.cs
+++ App2/MainPage.xaml.cs
@@ -40,7 +40,13 @@ namespace App2
         }
 
          Storyboard _gameLoop = new Storyboard();
+         bool upPressed = false;
+         bool downPressed = false;
 
+         int vx = -5, vy = 5; int _mx = 991, _my = 527;
+         int s1 = 0; int s2 = 0; int tm = 0;
+
+         bool paused = true;
 
          void keydown(Windows.UI.Core.CoreWindow win, Windows.UI.Core.KeyEventArgs e)
          {
@@ -85,7 +91,7 @@ namespace App2
 
         private void Initialise()
         {
-            _gameLoop.Duration = TimeSpan.FromMilliseconds(0);
+            _gameLoop.Duration = new Duration(new TimeSpan(0));
 
             //Once the duration has completed run the following method
             _gameLoop.Completed += GameLoop;
@@ -97,6 +103,18 @@ namespace App2
             _mx = (int) canvas.Width;
             _my = (int) canvas.Height;
 
+            _gameLoop.Begin();
+        }
+
+        private void GameLoop(object sender, object e)
+        {
+            //
+            //Execute all game logic
+
+            if (!paused)
+                GameLogic();
+
+            //restart our gameloop
             _gameLoop.Begin();
         }
 
@@ -112,8 +130,11 @@ namespace App2
             Canvas.SetLeft(ball, Canvas.GetLeft(ball) + vx);
             Canvas.SetTop(ball, Canvas.GetTop(ball) + vy);
 
-            if (Canvas.GetLeft(ball) > _mx || Canvas.GetLeft(ball) < 0) { vx *= -1; s2++; score2.Text = s2.ToString(); }
-            if (Canvas.GetTop(ball) > _my || Canvas.GetTop(ball) < 0) vy *= -1;
+            if (Canvas.GetLeft(ball) > _mx || Canvas.GetLeft(ball) < 0)
+                { vx *= -1; s2++; score2.Text = s2.ToString(); }
+
+            if (Canvas.GetTop(ball) > _my || Canvas.GetTop(ball) < 0)
+                vy *= -1;
 
             Canvas.SetTop(player2bat, Canvas.GetTop(ball) - player2bat.Height / 2);
             
@@ -132,6 +153,7 @@ namespace App2
                 vx *= -1;
             }
 
+            //gradually increase difficulty
             int x = tm / 5;
             score1.Text = x.ToString();
             vx /= Math.Abs(vx);
@@ -141,53 +163,6 @@ namespace App2
 
         }
 
-
-        private void GameLoop(object sender, object e)
-        {
-            TimeSpan delayTime = new TimeSpan(0, 0, 0, 0, 0);
-            //Take the start time
-            TimeSpan beginTime = new TimeSpan(
-     DateTime.Now.Day,
-     DateTime.Now.Hour,
-     DateTime.Now.Minute,
-     DateTime.Now.Second,
-     DateTime.Now.Millisecond);
-
-            //Execute all game logic
-
-            if (!paused)
-                GameLogic();
-
-            //set the delay to zero
-            TimeSpan msSpan = new TimeSpan(0);
-
-            //take the end time
-            TimeSpan endTime = new TimeSpan(
-    DateTime.Now.Day,
-    DateTime.Now.Hour,
-    DateTime.Now.Minute,
-    DateTime.Now.Second,
-    DateTime.Now.Millisecond);
-
-            //if the end time is less than the delay time + begin time add some delay, otherwise don’t add delay
-            if (endTime < beginTime + delayTime)
-            {
-                msSpan = ((beginTime + delayTime) - endTime);
-            }
-
-            //change the delay before the beginning of the next execution
-            _gameLoop.Duration = TimeSpan.FromMilliseconds(msSpan.TotalMilliseconds);
-            //restart our gameloop
-            _gameLoop.Begin();
-        }
-
-        bool upPressed = false;
-        bool downPressed = false;
-
-        int vx = -5, vy = 5; int _mx = 991, _my=527;
-        int s1 = 0; int s2 = 0; int tm = 0;
-
-        bool paused = true;
 
     }
 }
